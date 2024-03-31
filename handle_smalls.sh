7fdebe0d7fe37def2f8e1ae9836b897464d2f6cf
Chunt0 <chunt.mail@gmail.com>
2024-03-31 13:36:08 -0700

    2024-03-31 13:36:08

--- handle_smalls.sh
+++ handle_smalls.sh
@@ -14,6 +14,10 @@ for image_file in "$folder_path"/*; do
         # Extract width and height from dimensions
         width=$(echo "$dimensions" | cut -d'x' -f1)
         height=$(echo "$dimensions" | cut -d'x' -f2)
+        if [ "$width" -le 400 ] || [ "$height" -le 400 ]; then
+            # Resize the image to fit within 3000x3000 while preserving aspect ratio
+            rm -f "$image_file"
+        fi
         if [ "$width" -le 700 ] || [ "$height" -le 700 ]; then
             mv "$image_file" "$destination_folder"
         fi
