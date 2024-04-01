64402158bd9234953d0eb9d6503cad83b6a20b2c
Chunt0 <chunt.mail@gmail.com>
2024-03-31 19:24:43 -0700

    2024-03-31 19:24:43

--- target_prep.sh
+++ target_prep.sh
@@ -7,50 +7,39 @@ folder_path="./static/target"
 # Navigate to the folder containing the images
 cd "$folder_path" || exit
 
+# Remove any existing text files
+rm *.txt
+
+# Initialize counter
+count=1
 
 # Loop through each image file in the folder
-for file in *.{webp, WEBP}; do
-    # Check if the file is a regular file and ends with ".webp"
+for file in *.{webp,WEBP,jpg,JPG,jpeg,JPEG,png,PNG}; do
+    # Check if the file is a regular file
     if [ -f "$file" ]; then
-        # Convert the webp image to PNG
-        convert "$file" "${file%.*}.png"
-        # Remove the original webp file
-        rm "$file"
-    fi
-done
+        # Convert the webp images to PNG if applicable
+        if [[ "$file" == *.{webp,WEBP} ]]; then
+            convert "$file" "${file%.*}.png"
+            rm "$file"
+        fi
 
-# Remove any txt files
-for file in *.txt; do
-    rm "$file"
-done
-
-# Loop through each image file in the folder
-for file in *.{jpg,JPG,jpeg,JPEG,png,PNG}; do
-        # Check if the file is a regular file and ends with ".png"
-    if [ -f "$file" ]; then
         # Get the dimensions of the image
-        dimensions=$(identify -format "%wx%h" "$image_file")
+        dimensions=$(identify -format "%wx%h" "$file")
         # Extract width and height from dimensions
         width=$(echo "$dimensions" | cut -d'x' -f1)
         height=$(echo "$dimensions" | cut -d'x' -f2)
-        
-        ############################################
-        ##############CRITICAL######################
-        # Check if the largest dimension is greater than or equal to some cutoff value -> i.e. 2500
+
+        # Check if the largest dimension is greater than or equal to 2500
         if [ "$width" -ge 2500 ] || [ "$height" -ge 2500 ]; then
-            ############################################
-            ##############CRITICAL######################
-            # Resize the image to fit the desired ratio! (i.e. 50%x50%) 
-            convert "$file" -auto-orient -resize 61%x61% "$file"
+            # Resize the image while preserving aspect ratio
+            convert "$file" -auto-orient -resize 2500x2500\> "$file"
         fi
-        
-        # Counter for renaming files
-        count=1
-        # Rename the image file to .png format
+
+        # Rename the image file to .png format with sequential numbering
         mv "$file" "$count.png"
-        # Create a corresponding text file with the same name
         touch "$count.txt"
-        # Increment the counter
+        
+        # Increment counter
         ((count++))
     fi
 done
